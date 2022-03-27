67181d41eaea3c259d32e87512c0f2e29fec6b83
egooffice <myegooffice@gmail.com>
2022-03-27 02:42:05 +0100

    updated

---
+++ pages/_app.js
@@ -0,0 +1,27 @@
+// import '../styles/globals.css'
+
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+// export default MyApp
+
+import { useEffect } from 'react';
+import '../styles/globals.css';
+import { StoreProvider } from '../utils/Store';
+
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }, []);
+  return (
+    <StoreProvider>
+      <Component {...pageProps} />
+    </StoreProvider>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,63 @@
+import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@material-ui/core';
+import Layout from '../components/Layout';
+import data from '../utils/data';
+import NextLink from 'next/link';
+import db from '../utils/db';
+import Product from '../models/Product';
+
+export default function Home(props) {
+  const { products } = props;
+  return (
+    <Layout>
+      <div>
+        <h1>Product</h1>
+        <Grid container spacing={3}>
+          {products.map((product) => (
+            <Grid item md={4} key={product.name}>
+              <Card>
+                <NextLink href={`/product/${product.slug}`} passHref>
+                  <CardActionArea>
+                    <CardMedia
+                      component="img"
+                      image={product.image}
+                      title={product.name}
+                    ></CardMedia>
+                    <CardContent>
+                      <Typography>{product.name}</Typography>
+                    </CardContent>
+                  </CardActionArea>
+                </NextLink>
+                <CardActions>
+                  <Typography>${product.price}</Typography>
+                  <Button size="small" color="primary">
+                    Add to cart
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps() {
+  await db.connect();
+  const products = await Product.find({}).lean();
+  await db.disconnect();
+  return {
+    props: {
+      products: products.map(db.convertDocToObj),
+    },
+  };
+}
